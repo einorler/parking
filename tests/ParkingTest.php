0c2209a0de1266cc756064038ebb2c34245ced10
Mantas Marcinkevičius <marc.mantas@gmail.com>
2017-08-30 23:57:48 +0300

    red

--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -4,6 +4,7 @@ namespace Nfq\Tests;
 
 use Nfq\Factory\ParkingFactory;
 use Nfq\Model\Parking;
+use Nfq\Constants;
 use PHPUnit\Framework\TestCase;
 
 class ParkingTest extends TestCase
@@ -13,5 +14,11 @@ class ParkingTest extends TestCase
         $parking = ParkingFactory::create(120, 200);
 
         $this->assertInstanceOf(Parking::class, $parking);
+    }
+
+    public function testGetEmptySpaces()
+    {
+        $parking = ParkingFactory::create(120, 200);
+        $carArea = Constants::CAR_AREA;
     }
 }
