fd7d9721652894a9f59157984085ad3ae20c859a
Mantas Marcinkevičius <marc.mantas@gmail.com>
2017-08-30 23:43:14 +0300

    red

--- tests/ParkingTest.php
+++ tests/ParkingTest.php
@@ -2,9 +2,13 @@
 
 namespace Nfq\Tests;
 
+use Nfq\Factory\ParkingFactory;
 use PHPUnit\Framework\TestCase;
 
 class ParkingTest extends TestCase
 {
-
+    public function testParkingCreation()
+    {
+        $parking = ParkingFactory::create(120, 200);
+    }
 }
