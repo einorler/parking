8a507312f854739df194f0a027d5f284c030568b
Mantas Marcinkevičius <marc.mantas@gmail.com>
2017-08-31 00:08:39 +0300

    green

--- src/Model/Parking.php
+++ src/Model/Parking.php
@@ -2,7 +2,22 @@
 
 namespace Nfq\Model;
 
+use Nfq\Constants;
+
 class Parking
 {
+    private $width;
 
+    private $length;
+
+    public function __construct(int $width, int $length)
+    {
+        $this->width = $width;
+        $this->length = $length;
+    }
+
+    public function getEmptySpaces()
+    {
+        return floor($this->width * $this->length / Constants::CAR_AREA);
+    }
 }
