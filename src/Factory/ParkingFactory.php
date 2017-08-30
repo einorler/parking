eaddaa74a457f9259e2521cdba5fd171ee3de600
Mantas Marcinkevičius <marc.mantas@gmail.com>
2017-08-30 23:48:50 +0300

    green

--- src/Factory/ParkingFactory.php
+++ src/Factory/ParkingFactory.php
@@ -2,10 +2,12 @@
 
 namespace Nfq\Factory;
 
+use Nfq\Model\Parking;
+
 class ParkingFactory
 {
     public static function create($width, $length)
     {
-
+        return new Parking($width, $length);
     }
 }
